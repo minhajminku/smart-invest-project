9104f2adeccdfe2303061accc0bfabc4fd1c2c99
minhaj <minhajvallil@gmail.com>
2022-11-13 11:23:26 +0530

    Add project files.

---
+++ SmartInvestment/Database/SqlQueries.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartInvestment.Database
+{
+    class SqlQueries
+    {
+        public static string GetUserByName_Password(string username, string password) 
+        {
+            string query = $"SELECT * FROM User WHERE username = '{username}' AND password = '{password}' ";
+            return query;
+        }
+    }
+}
